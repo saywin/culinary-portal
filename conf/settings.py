43b7d755c9478be6f67c26dfa51d378462fa43c6
tiway <stilon670@gmail.com>
2024-11-02 18:12:08 +0200

    Added models "Category" and "Post"

--- conf/settings.py
+++ conf/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from os import path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-ucjl9654p*@%mu^3yt84j5@o$)n9))%&0lu@m%%3ex@g)ikjtu"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- cooking/models.py
+++ cooking/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Категорія новин"""
+
+    title = models.CharField(max_length=255)
+
+
+class Post(models.Model):
+    """Для постів"""
+
+    title = models.CharField(max_length=255)
+    content = models.TextField(default="Скоро буде стаття...")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to="photos/", blank=True, null=True)
+    watched = models.IntegerField(default=0)
+    is_published = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)

89302a8d107df0579598bb7a8030ca41ed22627b
tiway <stilon670@gmail.com>
2024-11-02 19:50:51 +0200

    Added classes PostAdmin and CategoryAdmin to admin.py

--- cooking/admin.py
+++ cooking/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from cooking.models import Category, Post
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "watched", "is_published", "category", "created_at", "updated_at", "get_photo"]
+    list_display_links = ["id", "title"]
+    list_editable = ["is_published"]
+    readonly_fields = ["watched"]
+    list_filter = ["is_published", "category", "created_at"]
+
+    def get_photo(self, obj):
+        photo = obj.photo
+        if photo:
+            print(photo.url)
+            return mark_safe(f"<img src='{photo.url}' width=75/>")
+
+    get_photo.short_description = "Мініатюра"

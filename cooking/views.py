b8533af0deb76c11aba84990f675e6a13a623320
tiway <stilon670@gmail.com>
2024-11-08 10:46:42 +0200

    create func category_list to views.py

--- cooking/views.py
+++ cooking/views.py
@@ -6,9 +6,22 @@ from .models import Post, Category
 def index(request: HttpRequest) -> HttpResponse:
     """Для головної сторінки"""
     posts = Post.objects.all()
-
+    categories = Category.objects.all()
     context = {
         "title": "Головна сторінка",
-        "posts": posts
+        "posts": posts,
+        "categories": categories,
+    }
+    return render(request, "cooking/index.html", context)
+
+
+def category_list(request: HttpRequest, pk: int) -> HttpResponse:
+    """Реакція на натискання кнопки категорії"""
+    posts = Post.objects.filter(category_id=pk)
+    categories = Category.objects.all()
+    context = {
+        "title": posts[0].category,
+        "posts": posts,
+        "categories": categories,
     }
     return render(request, "cooking/index.html", context)

27fa8f0c7587476400b6255cedc0a612bba8ce51
tiway <stilon670@gmail.com>
2024-11-08 16:14:53 +0200

    Implement logic user_login, user_logout, add_post to views.py

--- cooking/views.py
+++ cooking/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Category
 from django.db.models import F
+from .forms import PostAddForm, LoginForm
+from django.contrib.auth import login, logout
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -35,3 +37,41 @@ def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     Post.objects.filter(pk=pk).update(watched=F("watched") + 1)
     context = {"title": article.title, "post": article, "recommend": recommendation}
     return render(request, "cooking/article_detail.html", context)
+
+
+def add_post(request: HttpRequest) -> HttpResponse:
+    """Додавання статті від користувача"""
+    if request.method == "POST":
+        form = PostAddForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = Post.objects.create(**form.cleaned_data)
+            post.save()
+            return redirect("cooking:post_detail", post.pk)
+    else:
+        form = PostAddForm()
+    context = {"title": "Додати статтю", "form": form}
+    return render(request, "cooking/acticle_add_form.html", context)
+
+
+def user_login(request: HttpRequest) -> HttpResponse:
+    """Аутентифікація користувача"""
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("cooking:index")
+    else:
+        form = LoginForm()
+
+    context = {
+        "title": "Авторизація користувача",
+        "form": form
+    }
+    return render(request, "cooking/login_form.html", context)
+
+
+def user_logout(request: HttpRequest) -> HttpResponse:
+    """Вихід користувача"""
+    logout(request)
+    return redirect("cooking:index")

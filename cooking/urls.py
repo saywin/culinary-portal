8d3f6715831f6b204bf826cbfe91e4b19af381e2
tiway <stilon670@gmail.com>
2024-11-09 12:42:04 +0200

    Added path login and logout

--- cooking/urls.py
+++ cooking/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from cooking.views import index, category_list, post_detail, add_post
+from cooking.views import index, category_list, post_detail, add_post, user_login, user_logout
 
 app_name = "cooking"
 
@@ -8,5 +8,8 @@ urlpatterns = [
     path("", index, name="index"),
     path("category/<int:pk>/", category_list, name="category_list"),
     path("post/<int:pk>", post_detail, name="post_detail"),
-    path("add_arcticle/", add_post, name="add_post")
+    path("add_arcticle/", add_post, name="add_post"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+
 ]

648fcbcb920afe1296e60c59e75aa6c41b4a1483
tiway <stilon670@gmail.com>
2024-11-09 15:19:51 +0200

    Added RegistrationForm to forms.py

--- cooking/forms.py
+++ cooking/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
 
 class PostAddForm(forms.ModelForm):
@@ -19,11 +20,52 @@ class PostAddForm(forms.ModelForm):
 
 class LoginForm(AuthenticationForm):
     """Форма для аутентифікації користувача"""
+
     username = forms.CharField(
         label="Ім'я користувача",
-        widget=forms.TextInput(attrs={"class": "form-control"})
+        widget=forms.TextInput(attrs={"class": "form-control"}),
     )
     password = forms.CharField(
         label="Пароль",
-        widget=forms.PasswordInput(attrs={"class": "form-control"})
+        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+    )
+
+
+class RegistrationForm(UserCreationForm):
+    """Форма для реєстрації користувача"""
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    username = forms.CharField(
+        max_length=150,
+        label="Ім'я користувача",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Ім'я користувача"}
+        ),
+    )
+    email = forms.CharField(
+        max_length=150,
+        label="Email",
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Введіть ваш email"}
+        ),
+    )
+    password1 = forms.CharField(
+        max_length=150,
+        label="Пароль",
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Пароль"}
+        ),
+    )
+    password2 = forms.CharField(
+        max_length=150,
+        label="Підтвердження пароля",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Підтвердіть пароль",
+            }
+        ),
     )

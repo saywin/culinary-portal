b4c8938995c88fbe98d3746da211a561e392395e
tiway <stilon670@gmail.com>
2024-11-10 16:38:00 +0200

    Replaced some functions with classes to views.py

--- cooking/urls.py
+++ cooking/urls.py
@@ -1,22 +1,25 @@
 from django.urls import path
 
 from cooking.views import (
-    index,
-    category_list,
-    post_detail,
-    add_post,
     user_login,
     user_logout,
     user_register,
+    Index, ArticleByCategory,
+    PostDetail,
+    AddPost,
 )
 
 app_name = "cooking"
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("category/<int:pk>/", category_list, name="category_list"),
-    path("post/<int:pk>", post_detail, name="post_detail"),
-    path("add_arcticle/", add_post, name="add_post"),
+    # path("", index, name="index"),
+    path("", Index.as_view(), name="index"),
+    # path("category/<int:pk>/", category_list, name="category_list"),
+    path("category/<int:pk>/", ArticleByCategory.as_view(), name="category_list"),
+    # path("post/<int:pk>", post_detail, name="post_detail"),
+    path("post/<int:pk>", PostDetail.as_view(), name="post_detail"),
+    # path("add_arcticle/", add_post, name="add_post"),
+    path("add_arcticle/", AddPost.as_view(), name="add_post"),
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
     path("register/", user_register, name="register"),
--- cooking/views.py
+++ cooking/views.py
@@ -8,55 +8,103 @@ from django.contrib import messages
 from django.views import generic
 
 
-def index(request: HttpRequest) -> HttpResponse:
+# def index(request: HttpRequest) -> HttpResponse:
+#     """Для головної сторінки"""
+#     posts = Post.objects.all()
+#     categories = Category.objects.all()
+#     context = {
+#         "title": "Головна сторінка",
+#         "posts": posts,
+#         "categories": categories,
+#     }
+#     return render(request, "cooking/_index.html", context)
+
+
+class Index(generic.ListView):
     """Для головної сторінки"""
-    posts = Post.objects.all()
-    categories = Category.objects.all()
-    context = {
-        "title": "Головна сторінка",
-        "posts": posts,
-        "categories": categories,
-    }
-    return render(request, "cooking/_index.html", context)
+
+    model = Post
+    context_object_name = "posts"
+    template_name = "cooking/_index.html"
+    extra_context = {"title": "Головна сторінка"}
 
 
-def category_list(request: HttpRequest, pk: int) -> HttpResponse:
+# def category_list(request: HttpRequest, pk: int) -> HttpResponse:
+#     """Реакція на натискання кнопки категорії"""
+#     posts = Post.objects.filter(category_id=pk)
+#     categories = Category.objects.all()
+#     context = {
+#         "title": posts[0].category,
+#         "posts": posts,
+#         "categories": categories,
+#     }
+#     return render(request, "cooking/_index.html", context)
+
+
+class ArticleByCategory(Index):
     """Реакція на натискання кнопки категорії"""
-    posts = Post.objects.filter(category_id=pk)
-    categories = Category.objects.all()
-    context = {
-        "title": posts[0].category,
-        "posts": posts,
-        "categories": categories,
-    }
-    return render(request, "cooking/_index.html", context)
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            category_id=self.kwargs["pk"], is_published=True
+        )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()  # context = {}
+        category = Category.objects.get(pk=self.kwargs["pk"])
+        context["title"] = category.title
+        return context
 
 
-def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+# def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     """Сторінка статті"""
+#     article = Post.objects.get(pk=pk)
+#     recommendation = Post.objects.exclude(pk=pk).order_by("-watched")[:5]
+#     Post.objects.filter(pk=pk).update(watched=F("watched") + 1)
+#     context = {
+#         "title": article.title,
+#         "post": article,
+#         "recommend": recommendation,
+#     }
+#     return render(request, "cooking/_article_detail.html", context)
+
+class PostDetail(generic.DetailView):
     """Сторінка статті"""
-    article = Post.objects.get(pk=pk)
-    recommendation = Post.objects.exclude(pk=pk).order_by("-watched")[:5]
-    Post.objects.filter(pk=pk).update(watched=F("watched") + 1)
-    context = {
-        "title": article.title,
-        "post": article,
-        "recommend": recommendation,
-    }
-    return render(request, "cooking/_article_detail.html", context)
+    model = Post
+    template_name = "cooking/_article_detail.html"
+
+    def get_queryset(self):
+        post = Post.objects.filter(pk=self.kwargs["pk"])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()  # context = {}
+        Post.objects.filter(pk=self.kwargs["pk"]).update(watched=F("watched") + 1)
+        post = Post.objects.get(pk=self.kwargs["pk"])
+        recommend = Post.objects.exclude(pk=self.kwargs["pk"]).order_by("-watched")[:5]
+        context["title"] = post.title
+        context["recommend"] = recommend
+        return context
 
 
-def add_post(request: HttpRequest) -> HttpResponse:
+# def add_post(request: HttpRequest) -> HttpResponse:
+#     """Додавання статті від користувача"""
+#     if request.method == "POST":
+#         form = PostAddForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             post = Post.objects.create(**form.cleaned_data)
+#             post.save()
+#             return redirect("cooking:post_detail", post.pk)
+#     else:
+#         form = PostAddForm()
+#     context = {"title": "Додати статтю", "form": form}
+#     return render(request, "cooking/_acticle_add_form.html", context)
+
+class AddPost(generic.CreateView):
     """Додавання статті від користувача"""
-    if request.method == "POST":
-        form = PostAddForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = Post.objects.create(**form.cleaned_data)
-            post.save()
-            return redirect("cooking:post_detail", post.pk)
-    else:
-        form = PostAddForm()
-    context = {"title": "Додати статтю", "form": form}
-    return render(request, "cooking/_acticle_add_form.html", context)
+    form_class = PostAddForm
+    template_name = "cooking/_article_add_form.html"
+    extra_context = {"title": "Додати статтю"}
 
 
 def user_login(request: HttpRequest) -> HttpResponse:
